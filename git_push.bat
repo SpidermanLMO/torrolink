1f0d1d1447b9292190e2b9bb6cbaf3b1ae74239d
SpidermanLMO <laigno@gmail.com>
2026-06-12 00:25:44 -0500

    Retire profile-update admin function

--- git_push.bat
+++ git_push.bat
@@ -2,7 +2,7 @@
 cd /d C:\Users\Laign\TorroLink
 if exist ".git\index.lock" del ".git\index.lock"
 git add -A
-git commit -m "Homepage: add roofer card, fix dropdown, SEO update; ungate profile-update"
+git commit -m "Retire profile-update admin function"
 git push origin main
 echo.
 echo Done! Press any key to close.
