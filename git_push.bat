61350d059aaad91589f197cf0fa4433937a53401
SpidermanLMO <laigno@gmail.com>
2026-06-11 22:20:08 -0500

    Fix webhook body encoding; add manual order recovery function

--- git_push.bat
+++ git_push.bat
@@ -1,9 +1,9 @@
 @echo off
-cd /d "C:\Users\Laign\TorroLink"
+cd /d C:\Users\Laign\TorroLink
 if exist ".git\index.lock" del ".git\index.lock"
 git add -A
-git commit -m "Fix: replace incorrect face painter photo"
-git push
+git commit -m "Fix webhook body encoding; add manual order recovery function"
+git push origin main
 echo.
 echo Done! Press any key to close.
 pause
